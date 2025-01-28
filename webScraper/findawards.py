12f85761e35ae94ac6dd35ffbac59d9ebcacd9d8
Dan Williamson <123106596+WilliamsonDan@users.noreply.github.com>
2025-01-28 12:11:13 -0600

    Mostly complete
    The logic is mostly complete, it can find all of the most recent award files and will not find very old award files. It still needs to retrieve the files from the webserver and save them in the ./award/ directory.

--- webScraper/findawards.py
+++ webScraper/findawards.py
@@ -8,6 +8,9 @@ import re
 # Including the full directory name, so that the award html page and jpgs can be
 # downloaded easily.
 
+# Editing History
+# 20250128: Dan Williamson
+
 def main(argv):
 
   domain = "http://paccentraljc.org/"
@@ -20,33 +23,48 @@ def main(argv):
   with open(filename, 'r') as filelist:
     lines = filelist.readlines()
     for row in lines:
+      #print ("DEBUG: This row:{}".format(row.strip()))
       # ./[0.9]?: indicates a directory with awards in it
       # interesting case, there are several of these: ./20170801/20170619:
       regex = "^\.\/[\d\/]+:"
       if re.match(regex, row):
+        #currentdirectory = ""
         currentdirectory = row.strip()
         currentdirectory = currentdirectory[2:(len(currentdirectory)-1)] + "/"
-        print("DEBUG Found a directory:{}".format(currentdirectory))
+        print("DEBUG Found an award directory:{}".format(currentdirectory))
         
       # [0.9]?.html is an award html file - save this file and put it in ./awards/
+      
+      # DW developer's note: I figured out why this is giving me bad directory
+      # information. There are some directories that contain award files
+      # (dddddd.html) that are not recognized as award directories (/dddd/dddd:).
+      
+      # DW developer's note: When the end f a directory comes up, reset current
+      # directory. When an award is found and the current directory is rest, 
+      # reject the award.
+      
       regex = "\d+\.html"
-      if re.match(regex, row):
+      if re.match(regex, row.strip()):
         thisfile = row.strip()
-        # retrieve this file
-        fullname = domain + currentdirectory + thisfile
-        print("DEBUG Found an award:{}".format(fullname))
-        # save it in ./awards/
+        # retrieve this file if the current directory is not reset
+        if len(currentdirectory) > 1:
+          fullname = domain + currentdirectory + thisfile
+          print("DEBUG Found an award:{} in directory:{}|".format(thisfile,currentdirectory))
+          # save it in ./awards/
         
+      regex = "\d+\.jpg"  
       # [0.9]?.jpg is an award image file - save this one
       #if row is an award image file:
+      
       # retrieve the image
       # save it in ./awards/
       
       # a blank line indicates the end of a sub directory
       if len(row.strip()) < 1:
         # This is when we should reset any directory information
+        #print ("DEBUG found the end of directory:{}".format(row.strip()))
         currentdirectory = ""
-        #print ("DEBUG found the end of a directory".format(row))
+        
       # In any other circumstances, we should do nothing with the line
   
 if __name__ == "__main__":
