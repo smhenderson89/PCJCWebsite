00e70483ca0e5088b8757294cb0f551a01d90e59
Scott Henderson <scott.mc.henderson@gmail.com>
2025-02-26 20:41:49 -0800

    setting up pages

--- website/server.js
+++ website/server.js
@@ -8,10 +8,18 @@ app.set('view engine', 'ejs');
 // cors
 app.use(cors())
 
+// Server information
+const hostname = "127.0.0.1"
+const port = 8000
+
 // use res.render to load up an ejs view file
 
 // index page
 app.get('/', function(req, res) {
+  res.render('pages/index');
+});
+
+app.get('/home', function(req, res) {
   res.render('pages/index');
 });
 
@@ -20,9 +28,19 @@ app.get('/about', function(req, res) {
   res.render('pages/about');
 });
 
-// 
+// personnel page
+app.get('/personnel', function(req, res) {
+  res.render('pages/personnel')
+})
+
+// calendar page
+app.get('/calendar', function(req, res) {
+  res.render('pages/calendar')
+})
 
 
 
-app.listen(8080);
-console.log('Server is listening on port 8080');
+app.listen(port, hostname, function() {
+  console.log(`Server running at http://${hostname}:${port}`)
+
+});
